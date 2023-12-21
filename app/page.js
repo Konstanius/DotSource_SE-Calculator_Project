34cea852bd5f138b18e0200b26bec67c1e1f0483
Konstanius <earlygamerhd@gmail.com>
2023-12-21 19:34:27 +0100

    Changed icons
    Added static export configuration option
    
    Made parentheses to be automatically managed by the input

--- app/page.js
+++ app/page.js
@@ -12,10 +12,11 @@ export default function Home() {
     const [toolTipY, setToolTipY] = useState(0)
     const [currentPrompt, setCurrentPrompt] = useState('')
     const [screenWidth, setScreenWidth] = useState(0)
+    const [resetCursorPos, setResetCursorPos] = useState(-1)
 
     useEffect(() => {
         if (currentPrompt === '') {
-            setCurrentPrompt(germanPrompts[Math.floor(Math.random() * germanPrompts.length)])
+            setCurrentPrompt(inputHints[Math.floor(Math.random() * inputHints.length)])
 
             // set the screen width
             setScreenWidth(window.innerWidth)
@@ -33,8 +34,41 @@ export default function Home() {
         }
     }, [currentPrompt]);
 
+    useEffect(() => {
+        if (resetCursorPos !== -1) {
+            document.getElementById('input').setSelectionRange(resetCursorPos, resetCursorPos)
+            setResetCursorPos(-1)
+        }
+    }, [resetCursorPos])
+
     const onChangedTextField = (newValue) => {
+        let cursorPosition = document.getElementById('input').selectionStart
+        if (newValue.length === input.length + 1) {
+            let inputChar = newValue.charAt(cursorPosition - 1)
+            if (inputChar === '(') {
+                // insert a closing bracket
+                newValue = newValue.substring(0, cursorPosition) + ')' + newValue.substring(cursorPosition)
+                setResetCursorPos(cursorPosition)
+            } else if (inputChar === ')') {
+                // Check if the next character is also a closing bracket, if yes, remove it
+                if (newValue.charAt(cursorPosition) === ')') {
+                    newValue = newValue.substring(0, cursorPosition) + newValue.substring(cursorPosition + 1)
+                    setResetCursorPos(cursorPosition)
+                }
+            }
+        } else if (newValue.length === input.length - 1) {
+            let removedChar = input.charAt(cursorPosition)
+            if (removedChar === '(') {
+                let nextChar = newValue.charAt(cursorPosition)
+                if (nextChar === ')') {
+                    newValue = newValue.substring(0, cursorPosition) + newValue.substring(cursorPosition + 1)
+                    setResetCursorPos(cursorPosition)
+                }
+            }
+        }
+
         setInput(newValue)
+
         try {
             let data = parseWithParentheses(newValue, true)
             setValidity(true)
@@ -50,17 +84,59 @@ export default function Home() {
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <TextField
-                input={input}
-                onChanged={onChangedTextField}
-                onTooltipChanged={(tooltip, x, y) => {
-                    setTooltip(tooltip)
-                    setToolTipX(x)
-                    setToolTipY(y)
-                }}
-                screenWidth={screenWidth}
-                currentPrompt={currentPrompt}
-            />
+
+            <div className="flex flex-col items-center justify-center">
+                <input
+                    id="input"
+                    autoFocus={false}
+                    // no outline, no box shape etc., only shows the text
+                    className="bg-transparent text-white text-6xl p-4 m-4 text-center outline-none caret-red-500"
+                    type="text"
+                    style={{width: screenWidth * 0.8}}
+                    value={input}
+                    placeholder={currentPrompt}
+                    onChange={(e) => {
+                        onChangedTextField(e.target.value)
+                    }}
+                    onAbort={(_) => {
+                        setTooltip('')
+                        setToolTipX(0)
+                        setToolTipY(0)
+                    }}
+                    onInput={(_) => {
+                        setTooltip('')
+                        setToolTipX(0)
+                        setToolTipY(0)
+                    }}
+                    onBlur={(_) => {
+                        setTooltip('')
+                        setToolTipX(0)
+                        setToolTipY(0)
+                        document.getElementById('input').focus()
+                    }}
+                    onSelect={(e) => {
+                        // get the selected text
+                        let start = e.nativeEvent.target.selectionStart
+                        let end = e.nativeEvent.target.selectionEnd
+                        if (start === undefined || end === undefined || start === end) {
+                            setTooltip('')
+                            setToolTipX(0)
+                            setToolTipY(0)
+                            return
+                        }
+                        let text = e.nativeEvent.target.value.substring(start, end)
+                        let result = parseWithParentheses(text, true)
+
+                        let posX = e.nativeEvent.target.x
+                        let posY = e.nativeEvent.target.y
+
+                        // show the tooltip
+                        setTooltip(result.toLocaleString(navigator.language, {useGrouping: false}))
+                        setToolTipX(posX)
+                        setToolTipY(posY)
+                    }}
+                />
+            </div>
             {/*    the tooltip that moves with the mouse, if tooltip is empty, don't show it*/}
             {tooltip === '' ? '' :
                 <div className="absolute bg-gray-900 text-white p-2 rounded-md"
@@ -84,7 +160,7 @@ export default function Home() {
     )
 }
 
-const germanPrompts = [
+const inputHints = [
     "Fang an zu rechnen...",
     "Berechne das Ergebnis...",
     "Gib ein paar Formeln ein...",
@@ -104,50 +180,3 @@ const germanPrompts = [
     "Mathe war auch nie mein Lieblingsfach.",
     "Ich könnte es auch nicht besser...",
 ];
-
-function TextField({input, onChanged, onTooltipChanged, currentPrompt, screenWidth}) {
-    return (
-        <div className="flex flex-col items-center justify-center">
-            <input
-                id="input"
-                autoFocus={false}
-                // no outline, no box shape etc., only shows the text
-                className="bg-transparent text-white text-6xl p-4 m-4 text-center outline-none caret-red-500"
-                type="text"
-                style={{width: screenWidth * 0.8}}
-                value={input}
-                placeholder={currentPrompt}
-                onChange={(e) => {
-                    onChanged(e.target.value)
-                }}
-                onAbort={(_) => {
-                    onTooltipChanged('', 0, 0)
-                }}
-                onInput={(_) => {
-                    onTooltipChanged('', 0, 0)
-                }}
-                onBlur={(_) => {
-                    onTooltipChanged('', 0, 0)
-                    document.getElementById('input').focus()
-                }}
-                onSelect={(e) => {
-                    // get the selected text
-                    let start = e.nativeEvent.target.selectionStart
-                    let end = e.nativeEvent.target.selectionEnd
-                    if (start === undefined || end === undefined || start === end) {
-                        onTooltipChanged('', 0, 0)
-                        return
-                    }
-                    let text = e.nativeEvent.target.value.substring(start, end)
-                    let result = parseWithParentheses(text, true)
-
-                    let posX = e.nativeEvent.target.x
-                    let posY = e.nativeEvent.target.y
-
-                    // show the tooltip
-                    onTooltipChanged(result.toLocaleString(navigator.language, {useGrouping: false}), posX, posY)
-                }}
-            />
-        </div>
-    )
-}
